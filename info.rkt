bf7af9c55f946bc13cb26ab7a335a7204ff06030
Kevin Robert Stravers <macocio@gmail.com>
2018-07-27 15:56:15 +0200

    Add lens and threading to deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "spipe")
-(define deps '("base"))
+(define deps '("base" "lens" "threading"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/spipe.scrbl" ())))
 (define pkg-desc "Description Here")
