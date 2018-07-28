1d545da7d5ad5c1f6ce760201e68a011ea5bfef1
Kevin Robert Stravers <macocio@gmail.com>
2018-07-28 04:26:16 +0200

    Add sandbox-lib to build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "spipe")
 (define deps '("base" "lens" "threading"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/spipe.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.2.0")
