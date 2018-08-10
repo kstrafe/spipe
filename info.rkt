1c502ca7c977d91c3c40c8268eccd4d599648dbf
Kevin Robert Stravers <macocio@gmail.com>
2018-08-10 23:10:30 +0200

    Add package description

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base" "nested-hash" "threading"))
 (define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/spipe.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.5.0")
+(define pkg-desc "Pipeline-based programming using hash-tables")
+(define version "0.5.1")
 (define pkg-authors '("Kevin R. Stravers"))
